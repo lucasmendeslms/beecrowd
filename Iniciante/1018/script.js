9cbdb1d4d1e9e1ed2a4efb8ccab8224a24f7ef97
Lucas Mendes <lucas.mendessilva@hotmail.com>
2022-05-21 18:49:26 -0300

    Melhoria de código

--- Iniciante/1018/script.js
+++ Iniciante/1018/script.js
@@ -7,10 +7,10 @@ let moedas = [100, 50, 20, 10, 5, 2, 1];
 let notas = [0, 0, 0, 0, 0, 0, 0];
 
 for(let i = 0; i < moedas.length; i++){
-
+    
     if(valor >= moedas[i]){
         notas[i] = Math.floor(valor/moedas[i]);
-        valor = valor % moedas[i];
+        valor %= moedas[i];
     }  
 }
 
